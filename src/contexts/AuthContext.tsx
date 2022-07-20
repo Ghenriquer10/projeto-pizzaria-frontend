5556ac32306aff4f6c59541faa48c075ba9d1df2
Gustavo Henrique <ghenriquer10@gmail.com>
2022-07-20 12:44:05 -0300

    Criando logica do cadastro de usuário

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useState } from 'react';
-import { destroyCookie } from 'nookies';
+import { destroyCookie, setCookie, parseCookies } from 'nookies';
 import Router from 'next/router'
+import { api } from '../services/apiClient'
 
 type AuthContextData = {
     user: UserProps,
@@ -41,8 +42,36 @@ export function AuthProvider({ children }: AuthProviderPros) {
     const isAuthenticated = !!user;
 
     async function signIn({ email, password }: SingInProps) {
-        console.log(email)
-        console.log(password)
+        try {
+            const response = await api.post('/session', {
+                email,
+                password
+            })
+            //console.log(response.data)
+
+            const { name, id, token } = response.data
+
+            setCookie(undefined, '@nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // Expirar em um mês
+                path: "/"
+            })
+
+            setUser({
+                id,
+                name,
+                email
+            })
+
+            // Enviando token a todas futuras requisições
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+            // Redirecionar usuário para os últimos pedidos
+            Router.push('dashboard')
+
+
+        } catch (err) {
+            console.log("Erro ao acessar", err)
+        }
     }
 
     return (
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import style from './dashboard.module.scss';
+
+export default function Dashboard() {
+    return (
+        <div className={style.container}>
+            <h1 className={style.h1}>Dashboard</h1>
+        </div>
+    )
+}

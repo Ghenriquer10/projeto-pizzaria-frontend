8d140ba51eaf0db4166e315668579a8711bf07d2
Gustavo Henrique <ghenriquer10@gmail.com>
2022-07-25 18:07:28 -0300

    Adicionando preview para o upload de imagens

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,10 +1,31 @@
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, useState, ChangeEvent } from 'react'
 import Head from "next/head";
 import { Header } from "../../components/Header";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import styles from './product.module.scss'
+import { FiUpload } from 'react-icons/fi'
 
 export default function Product() {
+
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [imageAvatar, setImageAvatar] = useState(null)
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>) {
+        if (!e.target.files) {
+            return
+        }
+
+        const image = e.target.files[0];
+
+        if (!image) {
+            return
+        }
+
+        if (image.type === 'image/jpeg' || image.type === 'image/png') {
+            setImageAvatar(image);
+            setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+        }
+    }
 
     async function handleProduct(event: FormEvent) {
         event.preventDefault()
@@ -23,6 +44,21 @@ export default function Product() {
                 <main className={styles.container}>
                     <h1>Cadastrar produto</h1>
                     <form onSubmit={handleProduct} className={styles.form}>
+                        <label className={styles.labelAvatar}>
+                            <span>
+                                <FiUpload size={25} color={'#fff'} />
+                            </span>
+                            <input type={'file'} accept="image/png, image/jpeg" onChange={handleFile} />
+                            {avatarUrl && (
+                                <img
+                                    className={styles.preview}
+                                    src={avatarUrl}
+                                    alt='Foto do produto'
+                                    width={250}
+                                    height={250}
+                                />
+                            )}
+                        </label>
                         <select>
                             <option>Selecione a categoria</option>
                             <option>Bebida</option>

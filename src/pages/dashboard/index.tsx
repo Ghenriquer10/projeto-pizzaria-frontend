c52d2e5ab3a14ec5b2fdcfa50d07eb1b68c0b8c9
Gustavo Henrique <ghenriquer10@gmail.com>
2022-07-26 15:11:51 -0300

    Estilizando página de ultimos pedidos

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { canSSRAuth } from '../../utils/canSSRAuth';
 import Head from 'next/head';
 import style from './dashboard.module.scss';
 import { Header } from '../../components/Header';
+import { FiRefreshCcw } from 'react-icons/fi'
 
 export default function Dashboard() {
     return (
@@ -11,6 +12,26 @@ export default function Dashboard() {
             </Head>
             <div>
                 <Header />
+                <main className={style.container}>
+                    <div className={style.containerHeader}>
+                        <p>Últimos pedidos</p>
+                        <button><FiRefreshCcw size={25} color="#3fffa3" /></button>
+                    </div>
+                    <article className={style.listOrders}>
+                        <section className={style.listItem}>
+                            <button>
+                                <div className={style.tag}></div>
+                                <span>Mesa 50</span>
+                            </button>
+                        </section>
+                        <section className={style.listItem}>
+                            <button>
+                                <div className={style.tag}></div>
+                                <span>Mesa 40</span>
+                            </button>
+                        </section>
+                    </article>
+                </main>
             </div>
         </>
     )
